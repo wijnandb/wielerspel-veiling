f291a6c8acaa2ad0956596e097f137e5dd2dda2b
Wijnand Baretta <wb@retta.nl>
2021-12-22 21:55:03 +0100

    remove django-debug-toolbar

--- wielerspel/urls.py
+++ wielerspel/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
-import debug_toolbar
+#import debug_toolbar
 
 
 urlpatterns = [
@@ -11,5 +11,5 @@ urlpatterns = [
     #path('veiling-tweede-ronde/', include('veilingeentjes.urls')),
     path('results/', include('results.urls')),
     path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
+    #path('__debug__/', include(debug_toolbar.urls)),
 ]
